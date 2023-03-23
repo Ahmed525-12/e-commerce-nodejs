3f3baf5d9b8df1a372d016600e509e18afdf05fb
Ahmed525-12 <ahmedsayed5250057@gmail.com>
2023-03-23 23:40:38 +0300

    handle errors

--- src/modules/category/category_services.js
+++ src/modules/category/category_services.js
@@ -1,61 +1,65 @@
 const CategoryModel = require('./category_model');
 const slugify = require('slugify');
-exports.createCategories=async(req,res)=>{
-const{name}=req.body
-    const category=new CategoryModel({name,slug:slugify(name)});
-    await category.save();
-   return res.status(201).json(category)
-}
+const catchAsyncError= require('express-async-handler')
 
 
 
-exports.getCategories=async(req,res)=>{
-
-    const categories=await  CategoryModel.find({});
-  
-    if (!categories) {
-       return res.status(404).json({message:"not found"})
-      } else {
-        
-        return  res.status(201).json(categories)
-      }
-}
-
-
-exports.getCategory=async(req,res)=>{
-const{id}=req.params;
-    const category=await  CategoryModel.findById(id);
-  if (!category) {
-    return res.status(404).json({message:"not found"})
-  } else {
-    
-    return  res.status(201).json(category)
-  }
-}
+exports.createCategories=catchAsyncError(async(req,res)=>{
+  const{name}=req.body
+      const category=new CategoryModel({name,slug:slugify(name)});
+      await category.save();
+     return res.status(201).json(category)
+  })
 
 
 
-exports.updateCategory=async(req,res)=>{
-    const{id}=req.params;
-    const{name}=req.body;
-        const category=await  CategoryModel.findByIdAndUpdate(id,{name,slug:slugify(name)},{new:true});
-      if (!category) {
-        return res.status(404).json({message:"not found"})
-      } else {
-        
-        return  res.status(201).json(category)
-      }
-}
+exports.getCategories=catchAsyncError(async(req,res)=>{
+
+  const categories=await  CategoryModel.find({});
+
+  if (!categories) {
+     return res.status(404).json({message:"not found"})
+    } else {
+      
+      return  res.status(201).json(categories)
+    }
+})
 
 
-exports.deleteCategory=async(req,res)=>{
-    const{id}=req.params;
-   
-        const category=await  CategoryModel.findByIdAndDelete(id);
-      if (!category) {
-        return res.status(404).json({message:"not found"})
-      } else {
-        
-        return  res.status(201).json({message:"successfully deleted",category})
-      }
-}
+exports.getCategory=catchAsyncError(async(req,res)=>{
+  const{id}=req.params;
+      const category=await  CategoryModel.findById(id);
+    if (!category) {
+      return res.status(404).json({message:"not found"})
+    } else {
+      
+      return  res.status(201).json(category)
+    }
+  })
+
+
+
+exports.updateCategory=catchAsyncError(async(req,res)=>{
+  const{id}=req.params;
+  const{name}=req.body;
+      const category=await  CategoryModel.findByIdAndUpdate(id,{name,slug:slugify(name)},{new:true});
+    if (!category) {
+      return res.status(404).json({message:"not found"})
+    } else {
+      
+      return  res.status(201).json(category)
+    }
+})
+
+
+exports.deleteCategory=catchAsyncError(async(req,res)=>{
+  const{id}=req.params;
+ 
+      const category=await  CategoryModel.findByIdAndDelete(id);
+    if (!category) {
+      return res.status(404).json({message:"not found"})
+    } else {
+      
+      return  res.status(201).json({message:"successfully deleted",category})
+    }
+} )
